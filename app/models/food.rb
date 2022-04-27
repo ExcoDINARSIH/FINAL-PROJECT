2718f1c31e00c2b30c5cb5c2680ca2d6f6a2b4eb
Your Name <you@example.com>
2022-04-27 10:38:15 +0700

    Add validation for Food, invalid without price

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,4 @@
 class Food < ApplicationRecord
     validates :name, presence: true, uniqueness: true
+    validates :price, presence: true
 end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -25,19 +25,31 @@ RSpec.describe Food, type: :model do
 
   it "is invalid with a duplicate name" do
     food1 = Food.create(
-      name: "Nasi Buk",
-      description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+      name: 'Nasi Buk',
+      description: 'Madura special food consist of rice mixed bamboo shoots, fried empal, and jerky.',
       price: 23000.0
     )
     
     food2 = Food.new(
-      name: "Nasi Buk",
-      description: "Just with a different description.",
+      name: 'Nasi Buk',
+      description: 'Just with a different description.',
       price: 23000.0
     )
 
     food2.valid?
     
     expect(food2.errors[:name]).to include("has already been taken")
+  end
+
+  it 'is invalid without a price' do
+    food = Food.new(
+      name: 'Nasi Buk',
+      description: 'Madura special food consist of rice mixed bamboo shoots, fried empal, and jerky.',
+      price: nil
+    )
+
+    food.valid?
+
+    expect(food.errors[:price]).to include("can't be blank")
   end
 end
