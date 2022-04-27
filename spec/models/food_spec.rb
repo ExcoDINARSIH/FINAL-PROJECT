e9e56a3ad288835697eb76ec1c81c01b1d081849
Your Name <you@example.com>
2022-04-27 10:14:29 +0700

    Add customary spec for Food model

--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with a name and a description' do
+    food = Food.new(
+      name: 'Nasi Buk',
+      description: 'Madura special food consist of rice mixed bamboo shoots, fried empal, and jerky.',
+      price: 23000.0
+    )
+
+    expect(food).to be_valid
+  end
 end
