41b18198f220dea03149cee56c201471df9b8ded
Your Name <you@example.com>
2022-04-27 10:57:15 +0700

    Add faker to factories

--- spec/factories/foods.rb
+++ spec/factories/foods.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :food do
-    name { "Nasi Buk" }
-    description { "Madura special food consist of rice mixed bamboo shoots, fried empal, and jerky." }
-    price { 23000.0 }
+    name { Faker::Food.dish }
+    description { Faker::Food.description }
+    price { Faker::Number.positive }
   end
 end
